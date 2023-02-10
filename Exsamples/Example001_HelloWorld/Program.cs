330bdfbea7d8d4addc50ec60453655b987b3e5af
Kubinic <Osiptsov84@gmail.com>
2023-02-11 06:41:00 +0700

    вывод в терминал/консоль

---
+++ Exsamples/Example001_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Вывод текста" + " с " + "новой " + "строки" + "ИСКЛЮЧЕНИЕ");
+Console.WriteLine("Вывод текста");
+Console.WriteLine(" с ");
+Console.WriteLine("новой ");
+Console.WriteLine("строки");
+Console.Write("Вывод текста" + " в " + "одну " + "строку");
+Console.Write("Вывод текста");
+Console.Write(" в ");
+Console.Write("одну ");
+Console.Write("строку");
