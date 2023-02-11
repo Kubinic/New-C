5282f7de7feb2304e104c422549c4327faa60c04
Kubinic <Osiptsov84@gmail.com>
2023-02-11 09:59:48 +0700

    Сумма чисел + перевод int<=str

---
+++ Exsamples/Example003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine()!);
+int numberA = a;
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine()!);
+int numberB = b;
+int resalt = numberA + numberB;
+
+Console.WriteLine("Ответ " + resalt);
