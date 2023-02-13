64a1e923e5382ff9124ee9ba4474a478fdd71079
Kubinic <Osiptsov84@gmail.com>
2023-02-13 23:02:56 +0700

    Деление дробное число

--- Exsamples/Example004_Div_doble/Program.cs
+++ Exsamples/Example004_Div_doble/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число А: ");
+double a = double.Parse(Console.ReadLine()!);
+double numberA = a;
+Console.Write("Введите число B: ");
+double b = double.Parse(Console.ReadLine()!);
+double numberB = b;
+double resalt = numberA / numberB;
+
+Console.WriteLine("Ответ " + resalt);
