ff928ed9875252b03719a0c54a8b46faa970db7f
Kubinic <Osiptsov84@gmail.com>
2023-02-14 22:38:37 +0700

    Семинар практика

---
+++ 14.02.23/a3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+//  кратно ли оно одновременно 7 и 23.
+
+int num1 = new Random().Next(50, 100);
+Console.WriteLine($"{num1}");
+if((num1 % 7==0)&&(num1%23==0)){
+    Console.WriteLine("Кратное");
+}
+else
+{Console.WriteLine("не Кратное");}
---
+++ 14.02.23/a4/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int num1 = new Random().Next(0, 101);
+int num2 = new Random().Next(0, 101);
+Console.WriteLine($"{num1} {num2}");
+if(num1 == num2*num2){
+    Console.WriteLine($"{num2}Квадрат числа{num1}");
+}
+if(num2 == num1*num1){
+    Console.WriteLine($"{num2}Квадрат числа{num1}");
+
+}
+
+else
+{Console.WriteLine("Не одно число не является квадратом другого");}
