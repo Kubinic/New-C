ae973b106d8aadc828d77046fe1bb8972cf51026
Kubinic <Osiptsov84@gmail.com>
2023-02-14 10:10:59 +0700

    if/else_ToLower

---
+++ Exsamples/Example005_if-else/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите своё имя : ");
+string username = Console.ReadLine();
+if (username == "Ирина"){
+    Console.Write("Наконец то Ирина - добрый день!");
+}
+else {
+    Console.Write("Здравствуйте " + username);
+}
