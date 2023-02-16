b8903d4455c44abb271181ad4e98599cfd165a94
Kubinic <Osiptsov84@gmail.com>
2023-02-16 19:08:08 +0700

    Поиск индекса элемена в массиве arr[]

---
+++ lecture_2/Examle_02_arr/Program.cs
@@ -0,0 +1,12 @@
+﻿//Поиск идексе элемента "find"
+int[] array = {11, 2, 32, 36, 64, 4, 89, 91, 4};
+int n = array.Length;
+int index = 0;
+int find = 4;
+while (index < n){
+    if (array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
