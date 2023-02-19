f79c454f7e82634187a4fcd8ba8ddb7b927ef6e2
Kubinic <Osiptsov84@gmail.com>
2023-02-19 17:31:54 +0700

    добавил способ заполнения и способ вывода массиваarry

---
+++ lecture_2/Example_02/Program.cs
@@ -0,0 +1,10 @@
+﻿int [] arry = {12, 32, 5, 4, 76, 4, 13, 9, 16};
+int n = arry.Length;
+int index = 0;
+int find = 4;
+while(index < n){
+  if (find == arry[index]){
+    Console.WriteLine(index);
+    index++;
+  }
+}
---
+++ lecture_2/Example_03/Program.cs
@@ -0,0 +1,24 @@
+﻿void FillArray(int[] collection){
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArry(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count){
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+}
+
+
+int [] arry = new int[10];
+FillArray(arry);
+PrintArry(arry);
