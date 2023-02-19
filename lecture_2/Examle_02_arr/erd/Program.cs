a4e133750744c73f4a7e784f24f7af3832600f67
Kubinic <Osiptsov84@gmail.com>
2023-02-19 15:36:02 +0700

    лекция от17,02,23

---
+++ lecture_2/Examle_02_arr/erd/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 13: Напишите программу, которая выводит
+ //третью цифру заданного числа или сообщает, 
+ //что третьей цифры нет
+Console.Write("Введите число : ");
+int number = int.Parse(Console.ReadLine()!);
+if (number > 99 ){
+    number = number % 10;
+    Console.WriteLine(number);
+}
+else
+{Console.WriteLine("Третьей цифры нет");}
