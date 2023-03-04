004ec78e72b023d674bb5e141972e1de4e5ecd32
Kubinic <Osiptsov84@gmail.com>
2023-03-04 11:26:44 +0700

    Лекция 6

---
+++ 14.02.23/Seminar_6/Program.cs
@@ -0,0 +1,18 @@
+﻿// **Задача 42:**Напишите программу,
+//  которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите число : ");
+int num = int.Parse(Console.ReadLine()!);
+int a = 0;
+string r = "";
+for(int i = 0;num > 0;i++){
+    a = num %2;
+    num = num /2;
+    r = a +r;
+    
+    
+}
+Console.Write(r);
